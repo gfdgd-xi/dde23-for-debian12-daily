fbd6bc9711aae71d857cf2f412519cebbfe0a248
gfdgd_xi <3025613752@qq.com>
2023-10-04 10:47:53 +0800

    漏chroot

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@ if [[ ! -d /home/runner/debian12-chroot ]]; then
     # 构建 chroot 环境
     sudo apt install debootstrap qemu-user-static -y
     if [[ -f /tmp/arm64 ]]; then
-        debootstrap --arch=arm64 bookworm /home/runner/debian12-chroot
+        sudo debootstrap --arch=arm64 bookworm /home/runner/debian12-chroot
     else
-        debootstrap bookworm /home/runner/debian12-chroot    
+        sudo debootstrap bookworm /home/runner/debian12-chroot    
     fi
     sudo ./pardus-chroot /home/runner/debian12-chroot
 fi
