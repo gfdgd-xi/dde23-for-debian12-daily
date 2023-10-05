8a211773b2f3a712181969488140645fdb24c57a
gfdgd_xi <3025613752@qq.com>
2023-10-05 08:38:24 +0800

    漏改

--- run.sh
+++ run.sh
@@ -5,8 +5,20 @@ if [[ ! -d /home/runner/debian12-chroot ]]; then
     sudo apt install debootstrap qemu-user-static -y
     if [[ -f /tmp/arm64 ]]; then
         sudo debootstrap --arch=arm64 bookworm /home/runner/debian12-chroot
-    else
-        sudo debootstrap bookworm /home/runner/debian12-chroot    
+    else 
+        if [[ -f /tmp/mips64el ]]; then
+            sudo debootstrap --arch=mips64el bookworm /home/runner/debian12-chroot    
+        else
+            if [[ -f /tmp/riscv64 ]]; then
+                sudo debootstrap --arch=riscv64 sid /home/runner/debian12-chroot    
+            else
+                if [[ -f /tmp/loong64 ]]; then
+                    sudo debootstrap --no-check-gpg --arch=riscv64 sid /home/runner/debian12-chroot http://deb.debian.org/debian-ports
+                else
+                    sudo debootstrap bookworm /home/runner/debian12-chroot   
+                fi
+            fi
+        fi
     fi
     sudo ./pardus-chroot /home/runner/debian12-chroot
 fi
